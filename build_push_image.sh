5864046143cc086ff5cd5ad5a3057cea71a57b1e
RahmanIslamIen <rahmanislamien@gmail.com>
2023-09-09 07:41:16 +0700

    tahapan di perbaiki

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,7 +6,7 @@ image_tag="v1"
 docker build -t "rahmanislamien/item-app:v1" .
 
 #jika belum login
-#docker login -u USERNAME -p PASSWORD
+#docker login -u <USERNAME> -p <PASSWORD>
 
 #login tidak perlu karena sudah bisa login dari docker desktop sebelum nya
 
