f79a656fb9294ebe9c67bae0fe1ebca952996cf2
RahmanIslamIen <rahmanislamien@gmail.com>
2023-09-15 20:08:00 +0700

    di persimple

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# untuk username dan password nya buat login kalo belum login docker desktop
-# DOCKER_USERNAME="username"
-# DOCKER_PASSWORD="password"
-
-# bagian untuk nama repo dan nama image
-DOCKER_REPO="nama_repo"
 IMAGE_NAME="karsajobs-backend"
 
 # Build Docker image
 docker build -t $IMAGE_NAME .
 
-# jika belum login dari docker dekstop
-# docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-
-# membuat Tag image agar sesuai dangan nama repository
-docker tag $IMAGE_NAME $DOCKER_REPO/$IMAGE_NAME
-
-# Push image ke Docker Hub
-docker push $DOCKER_REPO/$IMAGE_NAME
